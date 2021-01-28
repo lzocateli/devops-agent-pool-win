c068f7651a6a77482b2f4f3b90493351bc986a26
CblDevOps <CblDevOps@cat.com>
2021-01-28 03:43:05 -0300

    Novas labels automaticas

---
+++ docker/push-docker-image.ps1
@@ -0,0 +1,23 @@
+# https://docs.microsoft.com/en-us/azure/devops/pipelines/agents/docker?view=azure-devops#windows
+
+[CmdletBinding()] 
+param(
+    [Parameter(Position = 0, Mandatory, ValueFromPipeline)]
+    [string]$dockerId,
+    [Parameter(Position = 1, Mandatory, ValueFromPipeline)]
+    [string]$dockerToken
+)
+
+Clear-Host
+
+docker login -u $dockerId -p $dockerToken
+
+imageName = "devops-agent-pool-win"
+$imageTag = "1.0.0"
+$DockerBuildT = "$dockerId/${imageName}:$imageTag"
+$DockerBuildLatest = "$dockerId/${imageName}:latest"
+
+docker tag $DockerBuildT $DockerBuildLatest
+
+docker push $DockerBuildT 
+docker push $DockerBuildLatest
