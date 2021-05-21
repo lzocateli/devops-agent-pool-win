566b15fe6aadb79c9eb77f8dea910cebbcb30b47
CblDevOps <CblDevOps@cat.com>
2021-05-21 17:27:53 -0300

    Incluido parametro wait para instalacao do az cli

--- agent/install-azcli.ps1
+++ agent/install-azcli.ps1
@@ -7,22 +7,26 @@ Write-Host "Start script: $localScript$scriptName at: $(Get-Date)"
 
 $scriptAzureCLI = {
 
+  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
   Invoke-WebRequest `
   -Uri https://aka.ms/installazurecliwindows `
-  -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+  -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; #rm .\AzureCLI.msi
   
 }
 
 $scriptAzDevOps = {
   
+  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
   az extension add --name azure-devops
-  az extension list
+  az extension list >azdevops.log
   
 }
 
 
-Write-Host "Donwloading and install AzureCLI"
-Start-Process powershell -ArgumentList "-noexit -command (Invoke-Command -ScriptBlock {$scriptAzureCLI})" -Verb runAs 
+Write-Host "Donwloading and install AzureCLI ..."
+$procAzCli = Start-Process powershell -ArgumentList "-noexit -command (Invoke-Command -ScriptBlock {$scriptAzureCLI})" -Verb runAs -Wait
+Write-Host "AzCli exit code: $($procAzCli.ExitCode)"
 
-Write-Host "Donwloading and install az devops"
-Start-Process powershell -ArgumentList "-noexit -command (Invoke-Command -ScriptBlock {$scriptAzDevOps})" -Verb runAs 
+Write-Host "Donwloading and install az devops ..."
+$procAzDvo = Start-Process powershell -ArgumentList "-noexit -command (Invoke-Command -ScriptBlock {$scriptAzDevOps})" -Verb runAs -Wait
+Write-Host "AzDevOps exit code: $($procAzDvo.ExitCode)"
