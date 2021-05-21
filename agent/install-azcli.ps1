13aab2a868b9cf276d8c43183edeea5f76489111
CblDevOps <CblDevOps@cat.com>
2021-05-21 14:18:50 -0300

    Instalando az usando powershel session

--- agent/install-azcli.ps1
+++ agent/install-azcli.ps1
@@ -26,8 +26,8 @@ $scriptAzDevOps = {
 
 #$so = New-PSSessionOption -SkipCACheck -IdleTimeout 7200000 -OperationTimeout 0 -OutputBufferingMode Block
 #$s = New-PSSession -ComputerName $serverName -SessionOption $so 
-$s = New-PSSession -ComputerName $env:COMPUTERNAME –RunAsAdministrator
+$s = New-PSSession -ComputerName $env:COMPUTERNAME
 Invoke-Command -Session $s -ScriptBlock $scriptAzureCLI
 
-$s1 = New-PSSession -ComputerName $env:COMPUTERNAME –RunAsAdministrator
+$s1 = New-PSSession -ComputerName $env:COMPUTERNAME
 Invoke-Command -Session $s1 -ScriptBlock $scriptAzDevOps
